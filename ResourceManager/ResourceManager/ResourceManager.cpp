744c8fd0063e25f50faa68805abdca920a44b239
dastyk <dastykill@gmail.com>
2016-09-30 09:45:31 +0200

    Free function in resource manager and test code. Seems to work.

--- ResourceManager/ResourceManager/ResourceManager.cpp
+++ ResourceManager/ResourceManager/ResourceManager.cpp
@@ -84,6 +84,26 @@ void ResourceManager::TestAlloc(void)
 	PrintOccupancy();
 	_Allocate(2);
 	PrintOccupancy();
+	_Allocate(15);
+	PrintOccupancy();
+	_Free(2, 3);
+	PrintOccupancy();
+	_Free(7, 1);
+	PrintOccupancy();
+	_Free(5, 1);
+	PrintOccupancy();
+	_Free(0, 1);
+	PrintOccupancy();
+	_Free(19, 1);
+	PrintOccupancy();
+	_Allocate(2);
+	PrintOccupancy();
+	_Free(9, 3);
+	PrintOccupancy();
+	_Free(14, 3);
+	PrintOccupancy();
+	_Allocate(3);
+	PrintOccupancy();
 }
 
 void ResourceManager::_Startup()
@@ -188,14 +208,106 @@ int ResourceManager::_Allocate(uint32_t blocks)
 	return allocSlot;
 }
 
+// Frees certain blocks by inserting them into the free block list.
 void ResourceManager::_Free(int32_t firstBlock, uint32_t numBlocks)
 {
+	// If there is no list to insert into, just make the current ones the new list.
+	if (_firstFreeBlock == -1)
+	{
+		_firstFreeBlock = firstBlock;
 
+		// Set up previous link on the first block
+		FreeBlock* b = reinterpret_cast<FreeBlock*>(_pool + firstBlock * _blockSize);
+		b->Previous = -1;
+
+		// For any other blocks we correctly set up the next link BEFORE actually
+		// getting the next block, after which we set up the previous link.
+		for (uint32_t block = firstBlock + 1; block < firstBlock + numBlocks; ++block)
+		{
+			b->Next = block;
+			b = reinterpret_cast<FreeBlock*>(_pool + block * _blockSize);
+			b->Previous = block - 1;
+		}
+
+		// The last block will now indicate the end of the list
+		b->Next = -1;
+	}
+	// First free block is after the ones we are returning
+	else if (_firstFreeBlock > firstBlock + numBlocks)
+	{
+		int32_t oldFirst = _firstFreeBlock;
+
+		// Insert at head of list (like above), with the exception that the last
+		// block points to the former first free block.
+		_firstFreeBlock = firstBlock;
+
+		// Set up previous link on the first block
+		FreeBlock* b = reinterpret_cast<FreeBlock*>(_pool + firstBlock * _blockSize);
+		b->Previous = -1;
+
+		// For any other blocks we correctly set up the next link BEFORE actually
+		// getting the next block, after which we set up the previous link.
+		for (uint32_t block = firstBlock + 1; block < firstBlock + numBlocks; ++block)
+		{
+			b->Next = block;
+			b = reinterpret_cast<FreeBlock*>(_pool + block * _blockSize);
+			b->Previous = block - 1;
+		}
+
+		// The last block will now point to the former head of the list
+		b->Next = oldFirst;
+		reinterpret_cast<FreeBlock*>(_pool + oldFirst * _blockSize)->Previous = firstBlock + numBlocks - 1;
+	}
+	// Existing free blocks before the ones we are returning. This means that
+	// we must find where in the list to insert and properly update the blocks
+	// before and after (if existing).
+	else
+	{
+		int32_t lastFree = _firstFreeBlock;
+		FreeBlock* lastFreeBlock = reinterpret_cast<FreeBlock*>(_pool + lastFree * _blockSize);
+		int32_t currFree = _firstFreeBlock;
+		FreeBlock* currFreeBlock = reinterpret_cast<FreeBlock*>(_pool + currFree * _blockSize);
+		
+		// Keep trying until we found correct spot (where the slot changed from immediately before to after)
+		while (currFree < firstBlock) // Less than comparison should be valid for entire range due to allocation list housekeeping
+		{
+			lastFree = currFree;
+			lastFreeBlock = currFreeBlock;
+
+			currFree = lastFreeBlock->Next;
+			if (currFree == -1)
+				break;
+
+			currFreeBlock = reinterpret_cast<FreeBlock*>(_pool + currFree * _blockSize);
+		}
+
+		lastFreeBlock->Next = firstBlock;
+
+		// Set up previous link on the first block
+		FreeBlock* b = reinterpret_cast<FreeBlock*>(_pool + firstBlock * _blockSize);
+		b->Previous = lastFree;
+
+		// For any other blocks we correctly set up the next link BEFORE actually
+		// getting the next block, after which we set up the previous link.
+		for (uint32_t block = firstBlock + 1; block < firstBlock + numBlocks; ++block)
+		{
+			b->Next = block;
+			b = reinterpret_cast<FreeBlock*>(_pool + block * _blockSize);
+			b->Previous = block - 1;
+		}
+
+		// Link to the next block (will be -1 as appropriate if end of pool)
+		b->Next = currFree;
+
+		// If we have another block, get it as link to the last returned block
+		if (currFree != -1)
+			reinterpret_cast<FreeBlock*>(_pool + currFree * _blockSize)->Previous = firstBlock + numBlocks - 1;
+	}
 }
 
 void ResourceManager::_Run()
 {
-	_numBlocks = 1000;
+	_numBlocks = 20;
 	_pool = new char[_numBlocks * _blockSize];
 
 	// Make blocks form a linked list (all of them at startup)
