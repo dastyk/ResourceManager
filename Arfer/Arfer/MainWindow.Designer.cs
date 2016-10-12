5d70882fcf1dd5cc53d59e8171967bed3f1179d9
Andreas Flöjt <andenzz@hotmail.com>
2016-10-12 12:58:18 +0200

    Loading and creating package

---
+++ Arfer/Arfer/MainWindow.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Arfer
+{
+    partial class Arfer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPackageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.existingPackageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.itemTree = new System.Windows.Forms.TreeView();
+            this.itemTreeRCCM = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemTreeNodeRCCM = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.itemTreeNodeRCCM.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newPackageToolStripMenuItem,
+            this.existingPackageToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newPackageToolStripMenuItem
+            // 
+            this.newPackageToolStripMenuItem.Name = "newPackageToolStripMenuItem";
+            this.newPackageToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.newPackageToolStripMenuItem.Text = "New Package";
+            this.newPackageToolStripMenuItem.Click += new System.EventHandler(this.newPackageToolStripMenuItem_Click);
+            // 
+            // existingPackageToolStripMenuItem
+            // 
+            this.existingPackageToolStripMenuItem.Name = "existingPackageToolStripMenuItem";
+            this.existingPackageToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.existingPackageToolStripMenuItem.Text = "Existing Package";
+            this.existingPackageToolStripMenuItem.Click += new System.EventHandler(this.existingPackageToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(158, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(158, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(0);
+            this.menuStrip1.Size = new System.Drawing.Size(613, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // itemTree
+            // 
+            this.itemTree.ContextMenuStrip = this.itemTreeRCCM;
+            this.itemTree.Location = new System.Drawing.Point(12, 27);
+            this.itemTree.Name = "itemTree";
+            this.itemTree.Size = new System.Drawing.Size(174, 521);
+            this.itemTree.TabIndex = 1;
+            this.itemTree.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.itemTree_AfterLabelEdit);
+            // 
+            // itemTreeRCCM
+            // 
+            this.itemTreeRCCM.Name = "itemTreeRCCM";
+            this.itemTreeRCCM.Size = new System.Drawing.Size(61, 4);
+            // 
+            // addFolderToolStripMenuItem
+            // 
+            this.addFolderToolStripMenuItem.Name = "addFolderToolStripMenuItem";
+            this.addFolderToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // itemTreeNodeRCCM
+            // 
+            this.itemTreeNodeRCCM.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renameToolStripMenuItem,
+            this.addFileToolStripMenuItem});
+            this.itemTreeNodeRCCM.Name = "itemTreeNodeRCCM";
+            this.itemTreeNodeRCCM.Size = new System.Drawing.Size(118, 48);
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.renameToolStripMenuItem.Text = "Rename";
+            // 
+            // addFileToolStripMenuItem
+            // 
+            this.addFileToolStripMenuItem.Name = "addFileToolStripMenuItem";
+            this.addFileToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.addFileToolStripMenuItem.Text = "Add File";
+            // 
+            // Arfer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(613, 560);
+            this.Controls.Add(this.itemTree);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Arfer";
+            this.Text = "Arfer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.itemTreeNodeRCCM.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TreeView itemTree;
+        private System.Windows.Forms.ContextMenuStrip itemTreeRCCM;
+        private System.Windows.Forms.ToolStripMenuItem addFolderToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip itemTreeNodeRCCM;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem existingPackageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newPackageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+    }
+}
+
---
+++ Arfer/Arfer/MainWindow.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Arfer
+{
+    public partial class Arfer : Form
+    {
+        private  string pathChoosen = "";
+        public Arfer()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void newPackageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if (itemTree.Nodes.Count != 0)
+                itemTree.Nodes.Clear();
+
+            pathChoosen = "";
+            TreeNode node = new TreeNode();
+            node.Text = "new package";
+            node.Name = "proot";
+            itemTree.Nodes.Add(node);
+
+            itemTree.SelectedNode = node;
+            itemTree.LabelEdit = true;
+            node.BeginEdit();
+            
+            saveToolStripMenuItem.Enabled = true;
+            saveAsToolStripMenuItem.Enabled = true;
+
+        }
+        private void existingPackageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog prom = new OpenFileDialog();
+            prom.Title = "Choose file to add";
+            prom.Filter = "Arfer Package|*.drf";
+            prom.ShowDialog();
+            if (File.Exists(prom.FileName))
+            {
+                itemTree.Nodes.Clear();
+                using (Stream file = File.Open(prom.FileName, FileMode.Open))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    object obj = bf.Deserialize(file);
+
+                    TreeNode[] nodeList = (obj as IEnumerable<TreeNode>).ToArray();
+                    itemTree.Nodes.AddRange(nodeList);
+                }
+
+                saveToolStripMenuItem.Enabled = true;
+                saveAsToolStripMenuItem.Enabled = true;
+            }
+           
+        }
+        
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(pathChoosen == "")
+            {
+                
+                SaveFileDialog prom = new SaveFileDialog();
+                prom.Title = "Choose were to save";
+                prom.Filter = "Arfer Package|*.drf";
+                prom.InitialDirectory = Environment.CurrentDirectory;
+                prom.ShowDialog();
+                pathChoosen = prom.FileName;
+            }
+
+            if (pathChoosen != "")
+            {
+                using (Stream file = File.Open(pathChoosen, FileMode.Create))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(file, itemTree.Nodes.Cast<TreeNode>().ToList());
+                }
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog prom = new SaveFileDialog();
+            prom.Title = "Choose were to save";
+            prom.Filter = "Arfer Package|*.drf";
+            prom.InitialDirectory = Environment.CurrentDirectory;
+            prom.ShowDialog();
+            pathChoosen = prom.FileName;
+
+
+            if (pathChoosen != "")
+            {
+                using (Stream file = File.Open(pathChoosen, FileMode.Create))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(file, itemTree.Nodes.Cast<TreeNode>().ToList());
+                }
+            }
+        }
+        private void itemTree_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            if (e.Label != null)
+            {
+                if (e.Label.Length > 0)
+                {
+                    if (e.Label.IndexOfAny(new char[] { '@', '.', ',', '!' }) == -1)
+                    {
+                        // Stop editing without canceling the label change.
+                        e.Node.EndEdit(false);
+                    }
+                    else
+                    {
+                        /* Cancel the label edit action, inform the user, and 
+                           place the node in edit mode again. */
+                        e.CancelEdit = true;
+                        MessageBox.Show("Invalid tree node label.\n" +
+                           "The invalid characters are: '@','.', ',', '!'",
+                           "Node Label Edit");
+                        e.Node.BeginEdit();
+                    }
+                }
+                else
+                {
+                    /* Cancel the label edit action, inform the user, and 
+                       place the node in edit mode again. */
+                    e.CancelEdit = true;
+                    MessageBox.Show("Invalid tree node label.\nThe label cannot be blank",
+                       "Node Label Edit");
+                    e.Node.BeginEdit();
+                }
+            }
+        }
+
+     
+    }
+}
